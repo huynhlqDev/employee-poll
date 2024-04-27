cffdca0d47f4ae4922502af6889dd3ab9a33749d
huynhlqDev <huynhlq.dev@gmail.com>
2024-04-27 10:56:06 +0700

    update get polls handler

--- src/actions/pollActions.js
+++ src/actions/pollActions.js
@@ -8,21 +8,22 @@ export const createPoll = (poll) => {
 };
 
 export const getPolls = () => {
-
     return async (dispatch) => {
-        
+
         dispatch({ type: 'SET_LOADING', payload: true })
 
-        try {
-            _getQuestions().then( questions => {
+        _getQuestions()
+            .then(questions => {
                 const value = Object.values(
                     Object.values(questions).map(item => item)
                 ).map(item => item)
                 dispatch({ type: 'GET_POLLS', payload: value })
+                dispatch({ type: 'SET_LOADING', payload: false })
+            }).catch(error => {
+                if (error.message) {
+                    console.log("error: ", error.message)
+                }
             })
-        } finally {
-            dispatch({ type: 'SET_LOADING', payload: false })
-        }
 
     };
 };
--- src/components/PollList.js
+++ src/components/PollList.js
@@ -11,27 +11,41 @@ const PollList = () => {
     const answerIdArray = Object.keys(userInfo.answers);
 
     useEffect(() => {
+        console.log("poll list:", polls)
+    }, [polls]);
+
+    useEffect(() => {
         dispatch(getPolls());
     }, [dispatch]);
+
+    const softTimestampToNewest = (a, b) => {
+        return b.timestamp - a.timestamp
+    }
+
 
     return (
         <div className='polls'>
             <div className='polls-section'>
                 <h3 className='polls-section-title'>New Questions</h3>
                 <div className='polls-section-body'>
-                    {polls.filter(poll => !answerIdArray.includes(poll.id))
-                    .map(poll => (
-                        <PollCard key={poll.id} poll={poll} />
-                    ))}
+                    {polls
+                        .filter(poll => !answerIdArray.includes(poll.id))
+                        .sort(softTimestampToNewest)
+                        .map(poll => (
+                            <PollCard key={poll.id} poll={poll} />
+                        ))
+                    }
                 </div>
             </div>
             <div className='polls-section'>
                 <h3 className='polls-section-title'>Done</h3>
                 <div className='polls-section-body'>
                     {polls.filter(poll => answerIdArray.includes(poll.id))
-                    .map(poll => (
-                        <PollCard key={poll.id} poll={poll} />
-                    ))}
+                        .sort(softTimestampToNewest)
+                        .map(poll => (
+                            <PollCard key={poll.id} poll={poll} />
+                        ))
+                    }
                 </div>
             </div>
         </div>
