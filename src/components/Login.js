a6c2e54f52f2568f6ea4cad33ded4e98252559c0
huynhlqDev <huynhlq.dev@gmail.com>
2024-05-06 23:10:25 +0700

    update css for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -50,33 +50,35 @@ const Login = () => {
 
     return (!isLoading &&
         <div className='login'>
-            <h1>Employee Polls</h1>
-            <img className='login-logo'
-                src="./login-logo-2.png"
-                alt={"logo"}
-            />
-            <div className='login-form'>
-                <div className="login-input">
-                    <input
-                        id='username'
-                        ref={usernameInput}
-                        type="text"
-                        placeholder="Username"
-                        onChange={handleUsernameChange}
-                        className="form-control"
-                    />
+            <div className='login-body'>
+                <h1 className='login-title'>Employee Polls</h1>
+                <img className='login-logo'
+                    src="./login-logo-2.png"
+                    alt={"logo"}
+                />
+                <div className='login-form'>
+                    <div className="login-input">
+                        <input
+                            id='username'
+                            ref={usernameInput}
+                            type="text"
+                            placeholder="Username"
+                            onChange={handleUsernameChange}
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="login-input">
+                        <input
+                            id='password'
+                            ref={passwordInput}
+                            type="password"
+                            placeholder="Password"
+                            onChange={handlePasswordChange}
+                            className="form-control"
+                        />
+                    </div>
+                    <button disabled={isDisabledSubmit} onClick={handleOnSubmit} className='login-form-btn'>Sign in</button>
                 </div>
-                <div className="login-input">
-                    <input
-                        id='password'
-                        ref={passwordInput}
-                        type="password"
-                        placeholder="Password"
-                        onChange={handlePasswordChange}
-                        className="form-control"
-                    />
-                </div>
-                <button disabled={isDisabledSubmit} onClick={handleOnSubmit} className='login-form-btn'>Submit</button>
             </div>
         </div>
     )
